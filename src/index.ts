5128dc2307baba370e69fff3ef036fb4a7f86efd
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-08-28 02:07:48 +0300

    file structure created

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+class UrlParser {
+  constructor(public url?: string) {}
+  init(): void {
+    // @ts-ignore-start
+    if (process.client || process.browser) {
+      // @ts-ignore-end
+      
+    }
+  }
+}
