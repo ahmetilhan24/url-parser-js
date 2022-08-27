8eea7118214baf57420890d9b6820649e45656df
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-08-28 02:17:30 +0300

    createdoop substructure

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
+import { URL } from "url";
+
 class UrlParser {
-  constructor(public url?: string) {}
+  constructor(public url?: URL) {
+    this.init();
+  }
   init(): void {
     // @ts-ignore-start
-    if (process.client || process.browser) {
+    if (process.client || process.browser || true) {
       // @ts-ignore-end
-      
+      console.log("asdasd");
     }
   }
+  public parseProtocol() {
+    console.log(this.url?.protocol);
+  }
 }
+
+const urlParser = new UrlParser(
+  new URL("https://www.linkedin.com/notifications")
+);
+
+urlParser.parseProtocol();
